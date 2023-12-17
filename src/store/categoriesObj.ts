23fa876bccf29c6c6b147cd7c868ba6eda9db0ba
Адилет Абдрасулов <adileto>
2023-12-17 21:59:38 +0600

    feat: Управление корзиной(добавление,удаление блюд из корзины)

--- src/store/categoriesObj.ts
+++ src/store/categoriesObj.ts
@@ -30,6 +30,6 @@ const categoriesObjSlice = createSlice({
 
 const { reducer: categoriesObjReducer, actions } = categoriesObjSlice;
 export const { categoriesObjReceved } = actions;
-//dsfsdfsdfsd
+//dsfsdfsываыавы
 export default categoriesObjReducer;
 
